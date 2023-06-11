cf307cfeaeae13a29113dfeaa752a2d2c5714b0f
MingHao0527 <wonminghao0527@e.neivce.edu.my>
2023-06-11 12:56:46 +0800

    Create hello_world.py
    asfd

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+def hello(to):
+    return f"Hello {to}!"
+
+def goodbye(to):
+    print("Please to meet you.")
+    return f"Goodbye {to}!"
+
+def main():
+    user = input("Name: ")
+    print(hello(user))
+    print(goodbye(user))
+          
+if __name__ == '__main__':
+    main()
